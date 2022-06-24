ffc3e380e4bdda6d12c5f5dc80b7ec0916f3a1c8
mananjabrik <manna.jabrik@gmail.com>
2022-06-25 03:19:03 +0700

    update

--- src/app.rs
+++ src/app.rs
@@ -5,8 +5,9 @@ pub fn app() -> Html {
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
+            <h1>{ "apa kabar" }</h1>
             <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <p>{ "This is a simple example of a Yew application." }</p>
         </main>
     }
 }
